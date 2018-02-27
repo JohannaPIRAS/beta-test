ef6353f5150266dff12e93052e503927fe726777
JOHANNA <joana.jojo@hotmail.fr>
2018-02-27 21:57:02 +0100

    ajout_datedujour

--- weather.js
+++ weather.js
@@ -1,35 +1,34 @@
 $(document).ready(function() {
-            $('#submitweather').click(function() {
-                    var city = $("#city").val();
-                    if (city != '') {
-                        $.ajax({
+    $('#submitweather').click(function() {
+        var city = $("#city").val();
+        if (city != '') {
+            $.ajax({
 
-                            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=248a8f723196ff7ffb7e6415ac07d252",
-                            type: "GET",
-                            dataType: "jsonp",
-                            success: function(data) {
-                                var widget = show(data);
+                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=248a8f723196ff7ffb7e6415ac07d252",
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data) {
+                    var widget = show(data);
 
-                                $("#show").html(widget);
-                                $("#city").val('');
+                    $("#show").html(widget);
+                    $("#city").val('');
 
-                                //console.log(data);
-                            }
-                        });
-
-                    } else {
-
-                        $("#error").html("Ce champ ne doit pas rester vide");
-                        }
-                    });
+                    //console.log(data);
+                }
             });
 
-        function show(data) {
-            return "<h3><strong>Temperature</strong>: " + data.main.temp + "&deg;C</h3>" +
-                "<h3><strong>MaximumTemperature</strong>: " + data.main.temp_max + "&deg;C</h3>" +
-                "<h3><strong>MinimumTemperature</strong>: " + data.main.temp_min + "&deg;C</h3>" +
-                "<h3><strong>Pressure</strong>: " + data.main.pressure + "hPa</h3>" +
-                "<h3><strong>WindSpeed</strong>: " + data.wind.speed + "m/s</h3>" +
-                "<h3><strong>Humidity</strong>: " + data.main.humidity + "%</h3>";
+        } else {
 
+            $("#error").html("Ce champ ne doit pas rester vide");
         }
+    });
+});
+
+function show(data) {
+    return + data.main.temp + "&deg;C" +
+        "<h4>MaximumTemperature: " + data.main.temp_max + "&deg;C</h4>" +
+        "<h4>MinimumTemperature: " + data.main.temp_min + "&deg;C</h4>" +
+        "<h4>Pression: " + data.main.pressure + "hPa</h4>" +
+        "<h4>Vent: " + data.wind.speed + "m/s</h4>" +
+        "<h4>Humidité: " + data.main.humidity + "%</h4>";
+}
