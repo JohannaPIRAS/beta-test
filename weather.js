7c93c2b4ab31e4a74e20d27b3dde0c39ec3b4c89
JOHANNA <joana.jojo@hotmail.fr>
2018-03-01 15:32:00 +0100

    ultime

--- weather.js
+++ weather.js
@@ -12,9 +12,13 @@ $(document).ready(function() {
                 type: "GET",
                 dataType: "jsonp",
                 success: function(data) {
+                    var temperature = tempactu(data);
                     var widget = show(data);
+                    var donnee = coordo(data);
 
+                    $("#tempactu").html(temperature);
                     $("#show").html(widget);
+                    $("#coordo").html(donnee);
                     $("#city").val('');
 
                     //console.log(data);
@@ -28,38 +32,45 @@ $(document).ready(function() {
     });
 });
 
+function tempactu(data) {
+    return "<h1> " + data.main.temp + "&deg;C</h1>";
+}
 function show(data) {
-    return "<h1 style='text-align:center; font-size:50px;'> " + data.main.temp + "&deg;C</h1>" +
-        "<h4>Max Temperature: " + data.main.temp_max + "&deg;C</h4>" +
+    return "<h4>Max Temperature: " + data.main.temp_max + "&deg;C</h4>" +
         "<h4>Min Temperature: " + data.main.temp_min + "&deg;C</h4>" +
         "<h4>Pression: " + data.main.pressure + "hPa</h4>" +
         "<h4>Vent: " + data.wind.speed + "m/s</h4>" +
         "<h4>Humidité: " + data.main.humidity + "%</h4>";
 }
 
+function coordo(data) {
+    return "<h4>Longitude: " + data.coord.lon + "</h4>" +
+        "<h4>Latitude : " + data.coord.lat + "</h4>";
+}
+
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 9,
-            center: { lat: 43.1148, lng: 1.608 }
-        });
-        var geocoder = new google.maps.Geocoder();
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: { lat: 43.1148, lng: 1.608 }
+    });
+    var geocoder = new google.maps.Geocoder();
 
-        document.getElementById('submit').addEventListener('click', function() {
-            geocodeAddress(geocoder, map);
-        });
-    }
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map);
+    });
+}
 
-    function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({ 'address': address }, function(results, status) {
-            if (status === 'OK') {
-                resultsMap.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: resultsMap,
-                    position: results[0].geometry.location
-                });
-            } else {
-                alert('Géolocalisation ne fonctionne pas pour la raison suivante: ' + status);
-            }
-        });
-    }
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Géolocalisation ne fonctionne pas pour la raison suivante: ' + status);
+        }
+    });
+}
